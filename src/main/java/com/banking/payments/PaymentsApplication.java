c0b054850174540cb7b2f4e7ed1151a551b93e66
krispis1 <belickaskristupas@outlook.com>
2022-07-28 19:59:54 +0300

    feat: added currency utility

--- src/main/java/com/banking/payments/PaymentsApplication.java
+++ src/main/java/com/banking/payments/PaymentsApplication.java
@@ -1,12 +1,15 @@
 package com.banking.payments;
 
+import com.banking.payments.util.CurrencyUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class PaymentsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(PaymentsApplication.class, args);
 	}
 
---
+++ src/main/java/com/banking/payments/util/CurrencyUtil.java
@@ -0,0 +1,34 @@
+package com.banking.payments.util;
+
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CurrencyUtil {
+
+    public enum Currency {
+        EUR, USD
+    }
+
+    public static double convert(Currency from, Currency to, Double amount) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+        Request request = new Request.Builder()
+                .url(String.format("https://api.apilayer.com/exchangerates_data/convert?to=%s&from=%s&amount=%f", to.toString(), from.toString(), amount))
+                .addHeader("apikey", "bU38NLcRRbg2zMBZoyHJHNnkLOczX0l1")
+                .build();
+        Response response = client.newCall(request).execute();
+
+        try {
+            Gson gson = new Gson();
+            Map responseMap = gson.fromJson(response.body().string(), Map.class);
+            return (double) responseMap.get("result");
+        } catch (NullPointerException ex) {
+            throw new NullPointerException(ex.getMessage());
+        }
+    }
+}
